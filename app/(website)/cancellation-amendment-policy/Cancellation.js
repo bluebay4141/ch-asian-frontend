58430c4a042e05ef32484180add9c67f6211349b
Ahamed Shamran <shamranahamed567@gmail.com>
2025-06-14 14:37:43 +0530

    fixes

--- app/(website)/cancellation-amendment-policy/Cancellation.js
+++ app/(website)/cancellation-amendment-policy/Cancellation.js
@@ -1,7 +1,7 @@
 function Cancellation({ settings }) {
   return (
     <article className="container mx-auto lg:px-25 my-10">
-      <p className="font-Helvetica text-base text-hero-3 font-semibold leading-5 text-center mb-5">
+      <p className="font-Helvetica text-base text-hero-3 font-semibold leading-5 mb-5">
         CANCELLATION & AMENDMENT POLICY
       </p>
       <p className="font-Helvetica text-base text-hero-3 font-normal leading-5">
